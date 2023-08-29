9b53af47f2ccb2c6e67eabf6094a3915ceebefab
Tony Baas <tonyrrbaas@gmail.com>
2023-08-29 12:56:51 -0500

    Initial Commit

---
+++ studentArcadeApp/studentArcadeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentArcadeAppApp.swift
+//  studentArcadeApp
+//
+//  Created by Tony Baas on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct studentArcadeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
