2a82e6f983dbabbcd64fdb0cc939e4a1cfc362f1
Tony Baas <tonyrrbaas@gmail.com>
2023-08-30 00:16:35 -0500

    Changes made: - Added foundation for splash screen (I still need to add our logo) - Created main contentView for menu screen

---
+++ studentArcadeApp/SplashView.swift
@@ -0,0 +1,49 @@
+//
+//  SplashView.swift
+//  studentArcadeApp
+//
+//  Created by Tony Baas on 8/29/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    var body: some View {
+        
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack{
+                    Image(systemName: "gamecontroller.fill")
+                        .font(.system(size: 80))
+                        .foregroundColor(.red)
+                    Text("Student Arcade")
+                        .font(.system(size: 20))
+                }
+                scaleEffect(size)
+                opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)){
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
--- studentArcadeApp/studentArcadeAppApp.swift
+++ studentArcadeApp/studentArcadeAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct studentArcadeAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
