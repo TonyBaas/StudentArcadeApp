bc329bb8de7b58e10488bda9fa4b5a904bb37d66
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-08-31 12:28:39 -0500

    Changes: - Added Hangman View

---
+++ studentArcadeApp/HangmanView.swift
@@ -0,0 +1,20 @@
+//
+//  HangmanView.swift
+//  studentArcadeApp
+//
+//  Created by Nicholas on 8/31/23.
+//
+
+import SwiftUI
+
+struct HangmanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HangmanView_Previews: PreviewProvider {
+    static var previews: some View {
+        HangmanView()
+    }
+}
