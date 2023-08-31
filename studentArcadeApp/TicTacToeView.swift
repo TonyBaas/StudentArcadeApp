4e3df32f4963cfa63635f5184633cd5a1c10e6d0
Connor Arends <Connor.arends@southeasttech.edu>
2023-08-31 10:22:56 -0500

    TicTacToe Page

---
+++ studentArcadeApp/TicTacToeView.swift
@@ -0,0 +1,20 @@
+//
+//  TicTacToeView.swift
+//  studentArcadeApp
+//
+//  Created by admin on 8/31/23.
+//
+
+import SwiftUI
+
+struct TicTacToeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TicTacToeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicTacToeView()
+    }
+}
