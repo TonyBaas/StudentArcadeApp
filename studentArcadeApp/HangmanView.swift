18a941f6eb1ec56d7939218c7ec99aaaba0c680f
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-08-31 12:41:29 -0500

    Changes: - Added VStack and added text inside of it

--- studentArcadeApp/HangmanView.swift
+++ studentArcadeApp/HangmanView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct HangmanView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Hello, World!")
+        }
     }
 }
 
