f4bdf6477270d2ccca9d7171ab6847f5ed577cb8
Connor Arends <Connor.arends@southeasttech.edu>
2023-08-31 10:32:04 -0500

    TicTacToe Page

--- studentArcadeApp/TicTacToeView.swift
+++ studentArcadeApp/TicTacToeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct TicTacToeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Tic Tac Toe!")
+            Image("")
+        }
+        
     }
 }
 
