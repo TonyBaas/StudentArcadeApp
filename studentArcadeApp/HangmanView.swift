526d5ac4dc5c9d38184860da4a31c6d95aabb895
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-09-04 17:19:22 -0500

    Added: Text box for title, text for title, changed font. Added: Spacers. Added: "Guess" button / Styling. Added: "Reset" button / Styling. Added: "@State private var" for management of multiple properties of the game. Added: "resetGame()" and "makeGuess()" functions (without the logic written in them). Added: Array of 16 words to guess from in game. Added: "var" under the array to go with the "@state private var" at the top.

--- studentArcadeApp/HangmanView.swift
+++ studentArcadeApp/HangmanView.swift
@@ -2,16 +2,65 @@
 //  HangmanView.swift
 //  studentArcadeApp
 //
-//  Created by Nicholas on 8/31/23.
+//  Created by Nicholas Woerner on 8/31/23.
 //
 
 import SwiftUI
 
 struct HangmanView: View {
+    @State private var chosenWord = ""
+    @State private var displayWord = ""
+    @State private var guess = ""
+    @State private var incorrectLetterArray = [Character]()
+    @State private var usedLetter = [Character]()
+    
+    
+    
     var body: some View {
         VStack{
-            Text("Hello, World!")
+            //Text heading and style for the game
+            Text("Hangman")
+                .font(.largeTitle)
+            Spacer()
+            
+            // Game Content below
+            Text("Guess a letter!")
+            Spacer()
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    //Guess button tap
+                    makeGuess()
+                }) {
+                    Text("Guess")
+                        .padding(13)
+                        .background(Color.black)
+                        .foregroundColor(Color.purple)
+                        .cornerRadius(10)
+                }
+                Button(action: {
+                    //Reset button tap
+                    resetGame()
+                }) {
+                    Text("Reset")
+                        .padding(13)
+                        .background(Color.black)
+                        .foregroundColor(Color.purple)
+                        .cornerRadius(10)
+                }
+                
+            }
         }
+  
+    }
+    
+    func makeGuess() {
+        //Guess logic
+    }
+    
+    func resetGame() {
+        //Reset Logic
     }
 }
 
@@ -20,3 +69,10 @@ struct HangmanView_Previews: PreviewProvider {
         HangmanView()
     }
 }
+
+
+    var wordArray = ["WATER", "SENTENCE", "APARTMENT", "BLANKET", "SWIFT", "PROGRAMMING", "BUTLER", "HIGHWAY", "AIRPORT", "MOUNTAIN", "NATURE", "FRIEND", "PROFESSOR", "SOUTHEAST", "COLLEGE", "DECEMBER"]
+    var chosenWord = ""
+    var displayArray = [Character]()
+    var displayWord = ""
+    var guess: Character!
